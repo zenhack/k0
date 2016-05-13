070a4a2f12f43dabc1e2a5c5107fd38942301650
Ian Denhardt <ian@zenhack.net>
2016-05-13 00:48:20 -0400

    Mess with the mboot header and such some more
    Grub isn't picking up the header. Looking at the built image it's
    obvious why: it's past the 8KiB mark. Before it is mostly zeros. Haven't
    figured out why that's happening though.

--- boot.s
+++ boot.s
@@ -1,16 +1,24 @@
 .global bsp_start
 .global mboot_header
 
+# Indicates the presence of the address fields in the multiboot header
+.set MBOOT_AOUT_KLUDGE, (1<<16)
+
 .set MBOOT_MAGIC, 0x1badb002
-.set MBOOT_FLAGS, 0
+.set MBOOT_FLAGS, MBOOT_AOUT_KLUDGE
 .set MBOOT_CHKSUM, (-(MBOOT_MAGIC + MBOOT_FLAGS))
 
 .align 4
 .section .mboot
 mboot_header:
-	.long MBOOT_MAGIC
-	.long MBOOT_FLAGS
-	.long MBOOT_CHKSUM
+	.int MBOOT_MAGIC
+	.int MBOOT_FLAGS
+	.int MBOOT_CHKSUM
+	.int mboot_header /* location of mboot header */
+	.int code         /* start of text segment */
+	.int bss          /* end of data section */
+	.int kend         /* end of bss */
+	.int bsp_start    /* entry point */
 .text
 .code32
 bsp_start:
