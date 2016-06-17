503a13bc23a506cbd5a32946320e100d6d984d46
Ian Denhardt <ian@zenhack.net>
2016-06-17 00:51:27 -0400

    Call rust code

--- boot64.s
+++ boot64.s
@@ -2,8 +2,15 @@
 .code64
 .global bsp_start64
 bsp_start64:
+	movq $_stack_base-8, %rsp
+	movq $0, %rbp
+	call bsp_main
+
 	movq $0x2f592f412f4b2f4f, %rax
 	movq %rax, 0xb8000
 hang:
 	hlt
 	jmp hang
+.bss
+.space 4096
+_stack_base:
