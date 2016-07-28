ffa1f88f5db29bc13bf08b4225f4e33ea998265b
Ian Denhardt <ian@zenhack.net>
2016-07-28 03:19:38 -0400

    Add commented-out writeln! call
    Might as well have this documented.

--- src/boot.rs
+++ src/boot.rs
@@ -9,6 +9,13 @@ pub extern fn bsp_main() {
   console::move_cursor(0, 0);
   serial::init(serial::COM1);
   serial::COM1.write_str("Hello\n").unwrap()
+
+    // This is currently expanding to something that calls
+    // ::std::somethingsomething, and moreover it's
+    // tripe-faulting the machine. I'm going to leave this
+    // commented out and not use writeln! and friends until
+    // I understand how they interact with bare-metal.
+//  writeln!(serial::COM1, "Hello, World!").unwrap()
 }
 
 // vim: set ts=2 sw=2 et :
