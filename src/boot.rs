37b7b050b27f3034045ae9b1a66be0ea9bb77aad
Ian Denhardt <ian@zenhack.net>
2017-05-30 21:04:55 -0400

    Restore calls to writeln
    ...now that my toolchain is working.

--- src/boot.rs
+++ src/boot.rs
@@ -11,22 +11,8 @@ pub extern fn bsp_main() {
     console.set_cell(4, 2, GREEN, BLACK, '!' as u8);
     console.move_cursor(0, 0);
     serial::init(serial::COM1);
-    serial::COM1.write_str("Hello\n").unwrap();
     let mut w = console::Writer::from_console(console, 0, 0, RED, BLACK);
-    unsafe { bochs::breakpoint() };
 
-    // Note to self: this is causing triple-faults with my toolchain.
-    // the reason is because rustc happens to generate SSE instructions
-    // with it (and doesn't if we leave it out). I need to set up a
-    // proper toolchain before I continue.
-    //
-    // The thing below is likely related.
-    w.write_str("Hello\n").unwrap()
-
-    // This is currently expanding to something that calls
-    // ::std::somethingsomething, and moreover it's
-    // tripe-faulting the machine. I'm going to leave this
-    // commented out and not use writeln! and friends until
-    // I understand how they interact with bare-metal.
-    //  writeln!(serial::COM1, "Hello, World!").unwrap()
+    writeln!(serial::COM1, "Hello, World!").unwrap();
+    writeln!(w, "Hello, World!").unwrap()
 }
