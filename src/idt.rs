5015ae8845ae5142fadeb5b20ff4f90e69b601e0
Ian Denhardt <ian@zenhack.net>
2017-06-02 16:49:08 -0400

    Remove old commented-out Gate implementation

--- src/idt.rs
+++ src/idt.rs
@@ -102,26 +102,3 @@ impl Gate {
     }
 
 }
-
-
-/*
-impl Gate {
-
-  pub fn unmarshal(&mut self, buf : &[u8; 16]) {
-    self.offset =
-      ( buf[0]  as u64)        |
-      ((buf[1]  as u64) << 8)  |
-      ((buf[6]  as u64) << 16) |
-      ((buf[7]  as u64) << 24) |
-      ((buf[8]  as u64) << 32) |
-      ((buf[9]  as u64) << 40) |
-      ((buf[10] as u64) << 48) |
-      ((buf[11] as u64) << 56) ;
-    self.selector = (buf[2] as u16) | ((buf[3] as u16) << 8);
-    self.ist = buf[4] & 0x7;
-    self.typ = buf[5] & 0xf;
-    self.dpl = (buf[5]>>5) & 0x3;
-    self.present = (buf[5] & (1<<7)) != 0;
-  }
-}
-*/
