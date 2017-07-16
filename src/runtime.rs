ee7bda2a9538beb19f5201d337c2e6ce141d582a
Ian Denhardt <ian@zenhack.net>
2017-07-16 17:20:15 -0400

    Clean up runtime.rs
    According to the libcore docs, this is all that is needed. The previous
    version was the result of various fiddling, so had a few unnecessary
    functions.

--- src/runtime.rs
+++ src/runtime.rs
@@ -4,18 +4,10 @@ extern crate rlibc;
 
 use core::fmt;
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
-
-#[no_mangle]
-#[allow(unused_variables)]
-pub extern fn rust_begin_panic(args : fmt::Arguments, file : &str, line : u32) -> ! {loop{}}
+#[lang = "eh_personality"]
+extern fn rust_eh_personality() {}
 
 #[no_mangle]
 #[allow(unused_variables)]
 #[lang = "panic_fmt"]
-pub extern fn rust_begin_unwind(args : fmt::Arguments, file : &str, line : u32) -> ! {loop{}}
-
-#[no_mangle]
-#[allow(non_snake_case)]
-pub extern "C" fn _Unwind_Resume() -> ! {loop{}}
-// vim: set ts=2 sw=2 et :
+pub extern fn rust_begin_panic(args : fmt::Arguments, file : &str, line : u32) -> ! {loop{}}
